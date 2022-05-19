84c73af3f60fe2b8032cd211be6b98a3522006d5
Immad khan <imadk727@gmail.com>
2022-05-19 16:51:35 +0300

    signup completed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,37 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import * as argon from 'argon2'
+import { AuthDto } from "./dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
-    constructor(private prismaService: PrismaService) {
+    constructor(private prisma: PrismaService) { }
 
-    }
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                }
+            })
+            delete user.hash
+            return user
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken you cant use it.')
+                }
+            }
+            throw error
 
-    signup() {
-        return {
-            message: ' this message form sign up'
         }
+
     }
 
-    signin() {
+    async signin() {
         return {
             message: ' this message form sign In'
         }
